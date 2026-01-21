9c666ae442ad34e70909b7a2574db606ff8e3b7c
LucasFelipeMartins <lucasmartinsfelipe321@gmail.com>
2026-01-20 22:38:23 -0300

    Buscas + Pagina de erro

--- src/controllers/buscaController.ts
+++ src/controllers/buscaController.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from "express";
+import { createMenu } from "../helpers/objectMenu";
+import { pets } from "../models/pets";
 
 export const busca = ((req:Request, res: Response) => {
 
-    //res.render('pages/busca');
+    let query:string = req.query.q as string;
+
+    if(!query){
+        res.redirect('/');
+        return;
+    }
+
+    let list = pets.getName(query);
+
+    res.render('pages/page', {
+        menu: createMenu(""),
+        list,
+        query
+    })
 
 })
